4e1c7a3d68e72c5190a3f7146b409728837da9a2
Lakshya Chaudhary <ChaudharyLakshya2@gmail.com>
2014-03-31 15:29:18 +0530

    page enter and leave animation added

--- Angular/todo/js/controllers.js
+++ Angular/todo/js/controllers.js
@@ -1,9 +1,32 @@
+app.controller('handlePageTransition',['$scope','$timeout','$location',function($scope,$timeout,$location){
+	$scope.currentRoute = window.location.href;
+	$scope.displayURL = function(){
+		var temp = $scope.currentRoute.split('/');
+		temp = temp[temp.length-1]
+		console.log(temp);
+		if(!temp.localeCompare('todo')){
+			$scope.todoAnimate = '';
+			$scope.imageAppAnimate = '';
+			window.location.href="/GitRepo/Angular/todo/#/imageapp";
+		}
+		
+	};
+	$scope.leftPage = function(){
+		$scope.pageTransition = 'slide-right';
+		$location.path('/imageapp');
+	};
+	$scope.rightPage = function(){
+		$scope.pageTransition = 'slide-left';
+		$location.path('/todo');
+	};
+}]);
 app.controller('listManagerController',['$scope','serverList',function($scope,serverList){
 	$scope.list = {"todo": []};
 	$scope.task;
 	$scope.newFileName;
 	$scope.addToList = function(){
 		$scope.list.todo.push($scope.task);
+		$scope.task = '';
 	};
 	$scope.openImageApp = function(){		    
 		window.location.href="/GitRepo/Angular/todo/#/imageapp";
